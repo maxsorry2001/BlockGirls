74fb9eaae3aaa53ff6b0f9a2774f34495820e652
1=G <maxsorry2001@163.com>
2026-09-05 17:06:33 +0800

    init

---
+++ src/main/java/net/gmaj7/block_girls/BlockGirlsClient.java
@@ -0,0 +1,24 @@
+package net.gmaj7.block_girls;
+
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+// This class will not load on dedicated servers. Accessing client side code from here is safe.
+@Mod(value = BlockGirls.MODID, dist = Dist.CLIENT)
+// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+@EventBusSubscriber(modid = BlockGirls.MODID, value = Dist.CLIENT)
+public class BlockGirlsClient {
+    public BlockGirlsClient(ModContainer container) {
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+    }
+}
